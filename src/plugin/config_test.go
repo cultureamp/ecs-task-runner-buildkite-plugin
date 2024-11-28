6a5001d78d73ddaa8deb7c0ca0fa2e1d10df1155
Sam Craske <sam.craske@cultureamp.com>
2024-11-28 11:26:08 +1100

    feat: plugin.RUN is the entrypoint of the business logic

--- src/plugin/config_test.go
+++ src/plugin/config_test.go
@@ -25,10 +25,12 @@ func TestFetchConfigFromEnvironment(t *testing.T) {
 	var config plugin.Config
 	fetcher := plugin.EnvironmentConfigFetcher{}
 
-	t.Setenv("BUILDKITE_PLUGIN_EXAMPLE_GO_MESSAGE", "test-message")
+	t.Setenv("BUILDKITE_PLUGIN_EXAMPLE_GO_PARAMETER_NAME", "test-parameter")
+	t.Setenv("BUILDKITE_PLUGIN_EXAMPLE_GO_SCRIPT", "hello-world")
 
 	err := fetcher.Fetch(&config)
 
 	assert.Nil(t, err, "fetch should not error")
-	assert.Equal(t, config.Message, "test-message", "fetched message should match environment")
+	assert.Equal(t, config.ParameterName, "test-parameter", "fetched message should match environment")
+	assert.Equal(t, config.Script, "hello-world", "fetched message should match environment")
 }
